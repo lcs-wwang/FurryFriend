b35a99ca8ac56762c8ddc112d93f19823e9f1975
winstonwang <wwang@lcs.on.ca>
2022-02-28 08:57:40 -0500

    Initial Commit

---
+++ FurryFriend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FurryFriend
+//
+//  Created by Winston Wang on 2022-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurryFriend/FurryFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurryFriendApp.swift
+//  FurryFriend
+//
+//  Created by Winston Wang on 2022-02-28.
+//
+
+import SwiftUI
+
+@main
+struct FurryFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
